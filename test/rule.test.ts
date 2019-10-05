9a56ffbf032217c6f0bce5c2a653bfee3e994143
Václav Volhejn <vaclav.volhejn@gmail.com>
2019-10-05 14:14:44 +0200

    Add OccurrenceRule

--- test/rule.test.ts
+++ test/rule.test.ts
@@ -4,6 +4,7 @@ import 'mocha'
 import '../src/rules/regExpEvent'
 import { RegExpEvent } from "../src/rules/regExpEvent"
 import { Ingredient } from "../src/misc/ingredient"
+import { OccurrenceRule } from "../src/rules/occurrenceRule"
 
 let s = [
   Ingredient.Tomato,
@@ -31,5 +32,26 @@ describe('RegExpEvent',
     it('works with $', () => {
       let event = new RegExpEvent('e$')
       expect(event.count(s)).to.equal(0)
+    });
+  })
+
+
+describe('OccurrenceRule',
+  () => {
+    it('both bounds, false', () => {
+      let rule = new OccurrenceRule(new RegExpEvent('t'), 1, 2)
+      expect(rule.acceptable(s)).to.equal(false)
+    });
+    it('both bounds, true', () => {
+      let rule = new OccurrenceRule(new RegExpEvent('t'), 3, 5)
+      expect(rule.acceptable(s)).to.equal(true)
+    });
+    it('only min, false', () => {
+      let rule = new OccurrenceRule(new RegExpEvent('t'), 5, undefined)
+      expect(rule.acceptable(s)).to.equal(false)
+    });
+    it('only min, true', () => {
+      let rule = new OccurrenceRule(new RegExpEvent('t'), 3, undefined)
+      expect(rule.acceptable(s)).to.equal(true)
     });
   })
