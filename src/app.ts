7cd2d215a292e53e434b1c7fade4208ec2c0b568
Vilém Zouhar <vilem.zouhar@gmail.com>
2019-10-05 11:58:46 +0200

    ingredient

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,9 @@ import { MainScene } from './scenes/main'
 let gameConfig : object = {
   title: "Untitled untitled Game",
   width: 900,
-  height: 300,
+  height: 450,
   parent: "game_canvas",
-  backgroundColor: "#18216D",
+  backgroundColor: "#bda4a4",
   scene: [MainScene],
 }
 
--- src/scenes/main.ts
+++ src/scenes/main.ts
@@ -12,12 +12,15 @@ export class MainScene extends Phaser.Scene {
 
     preload() {
         this.load.image('skewer', 'assets/skewer.png');
+        this.load.spritesheet('ingredient', 'assets/ingredient.png', { frameWidth: 64, frameHeight: 64 });
     }
 
     create() {
         // background
         let skewer = this.add.image(450, 100, 'skewer')
-        skewer.setDisplaySize(800, 80)
+        let randomitem = this.add.image(450, 100, 'ingredient')
+        randomitem.setFrame(0)
+        // skewer.setDisplaySize(800, 80)
 
         this.ballManager = new BallManager(this)
     }
