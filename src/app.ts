a25e490eaeb0b7eb57c19a155f70d64270c8571e
Vilém Zouhar <vilem.zouhar@gmail.com>
2019-10-05 17:12:45 +0200

    web formatting

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
 import "phaser"
 import { MainScene } from './scenes/main'
 
-let gameConfig : object = {
-  title: "Untitled Untitled Game",
-  width: 900,
-  height: 400,
-  parent: "game_canvas",
-  backgroundColor: "#bda4a4",
-  scene: [MainScene],
+let gameConfig: object = {
+    title: "Untitled Untitled Game",
+    width: 900,
+    height: 400,
+    parent: "game_canvas",
+    backgroundColor: "#bda4a4",
+    scene: [MainScene],
 }
 
 window.onload = () => {
-  var game = new Phaser.Game(gameConfig)
+    var game = new Phaser.Game(gameConfig)
 }
