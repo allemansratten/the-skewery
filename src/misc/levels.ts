b121d3b52c6e9f4b7232ab78d77f3fa0ef4d6549
Václav Volhejn <vaclav.volhejn@gmail.com>
2019-10-06 11:47:24 +0200

    Add a real level

--- src/misc/levels.ts
+++ src/misc/levels.ts
@@ -1,9 +1,11 @@
 import { Level } from "../rules/level"
 import { RegExpEvent } from "../rules/regExpEvent"
 import { OccurrenceRule } from "../rules/occurrenceRule"
+import { CompositeRule } from "../rules/compositeRule"
+import { PalindromeEvent } from "../rules/palindromeEvent"
 
 
-export let levels: Level[] = [
+export let levels : Level[] = [
     // Uvodni, seznami hrace s prvky na obrazovce
     new Level([
         new OccurrenceRule(new RegExpEvent("t"), 1, undefined,
@@ -20,15 +22,32 @@ export let levels: Level[] = [
     ], 1),
     // TODO cca 10-15 levelů, podle toho co zvládne Vašek
     new Level([
-        new OccurrenceRule(new RegExpEvent("e"), 2, undefined,
-            "must have at least two lileks"),
-        new OccurrenceRule(new RegExpEvent("ee"), undefined, 0,
-            "must not have two consecutive lileks"),
+        new CompositeRule([
+            new OccurrenceRule(new RegExpEvent('....'), undefined, 0),
+            new OccurrenceRule(new RegExpEvent('p'), 1, 1),
+        ], "at most 3 items, exactly one p"),
+        new OccurrenceRule(new RegExpEvent('e'), 0, 1, "at most 1 e"),
+        new CompositeRule([
+            new OccurrenceRule(new RegExpEvent('p'), 2, undefined),
+            new OccurrenceRule(
+                new RegExpEvent('.p.'), 1, undefined,
+                "at least one pepper not at the edge"
+            ),
+        ], "at least two peppers, from which at least one not at the edge"),
+        new CompositeRule([
+            new OccurrenceRule(new RegExpEvent('(^|[^e])p($|[^e])'),
+                undefined, 0, "p must be adjacent to >= 1 e"),
+            new OccurrenceRule(new RegExpEvent('epe'), undefined, 0),
+        ], "there must be exactly one e next to each p"),
+        new CompositeRule([
+            new OccurrenceRule(new PalindromeEvent(), 1, undefined),
+            new OccurrenceRule(new RegExpEvent('^.$|^...$'), 1, undefined),
+        ], "palindrome of an odd length"),
     ], 2),
     new Level([
-        new OccurrenceRule(new RegExpEvent("o"), 2, undefined,
-            "must have at least two cibulas"),
-        new OccurrenceRule(new RegExpEvent("oo"), undefined, 0,
-            "must not have two consecutive cibulas"),
+        new OccurrenceRule(new RegExpEvent("t"), 2, undefined,
+            "must have at least two tomatoes"),
+        new OccurrenceRule(new RegExpEvent("tt"), undefined, 0,
+            "must not have two consecutive tomatoes"),
     ], 1),
 ]
