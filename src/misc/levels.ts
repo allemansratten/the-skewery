143d529ee78fc5fc4edf53439786e2906901f9da
Václav Volhejn <vaclav.volhejn@gmail.com>
2019-10-06 16:25:51 +0200

    Honza's level 2 (work in progress)

--- src/misc/levels.ts
+++ src/misc/levels.ts
@@ -88,6 +88,42 @@ export let levels: Level[] = [
             new OccurrenceRule(new RegExpEvent('ete'), undefined, 0),
         ], "each tomato must be adjacent to exactly one eggplant"),
     ], 2, 4),
+    // Sachova uloha 2 (WIP)
+    new Level([
+        new OccurrenceRule(new RegExpEvent('pet'), 1, undefined,
+            'must contain the sequence "pepper - eggplant - tomato"'),
+        new OccurrenceRule(new RegExpEvent('^p|p$'), 1, undefined,
+            'must have a pepper on at least one of the edges'),
+        new OccurrenceRule(new RegExpEvent('op|po'), 1, undefined,
+            "there must be a pepper which is adjacent to an onion"),
+        new OccurrenceRule(new RegExpEvent('^(tt|ee|pp|oo)*$'), 1, 1,
+            "ingredients form pairs (each item must be adjacent to exactly one item of the same ingredient)"),
+        new CompositeRule([
+            new OccurrenceRule(new RegExpEvent('.'), 3, 3),
+            new OccurrenceRule(new UniqueIngredientsEvent(), 3, 3),
+        ], "must have exactly three items and their ingredients must all be different"),
+        new CompositeRule([
+            new OccurrenceRule(new RegExpEvent('(^|[^e])t($|[^e])'), undefined, 0),
+            new OccurrenceRule(new RegExpEvent('ete'), undefined, 0),
+        ], "each tomato must be adjacent to exactly one eggplant"),
+    ], 2, 4),
+    // Sachova uloha 3
+    // new Level([
+    //     new OccurrenceRule(new RegExpEvent('^p.*p$'), 1, 1,
+    //         "must have a pepper on both edges"),
+    //     new OccurrenceRule(new RegExpEvent('[^o]p[^o]'), 0, 0,
+    //         "peppers must be adjacent to onions on both sides"),
+    //     new OccurrenceRule(new RegExpEvent('^(tt|ee|pp|oo)*$'), 1, 1,
+    //         "ingredients form pairs (each item must be adjacent to exactly one item of the same ingredient)"),
+    //     new CompositeRule([
+    //         new OccurrenceRule(new RegExpEvent('.'), 3, 3),
+    //         new OccurrenceRule(new UniqueIngredientsEvent(), 3, 3),
+    //     ], "must have exactly three items and their ingredients must all be different"),
+    //     new CompositeRule([
+    //         new OccurrenceRule(new RegExpEvent('(^|[^e])t($|[^e])'), undefined, 0),
+    //         new OccurrenceRule(new RegExpEvent('ete'), undefined, 0),
+    //     ], "each tomato must be adjacent to exactly one eggplant"),
+    // ], 2, 4),
     // Dummy uloha na konci (technicke duvody)
     new Level([], 0),
 ]
